6b12fbd7708568adf698d5a519d580610b9dffb7
Jake Wilson <jake.e.wilson@gmail.com>
2016-11-05 10:30:43 -0600

    Added Cache constructor and put tests
    Signed-off-by: Jake Wilson <jake.e.wilson@gmail.com>

--- test/test.js
+++ test/test.js
@@ -38,3 +38,73 @@ describe('ExtensionContextMock', function () {
   });
 
 });
+
+describe('Cache', function () {
+
+  describe('constructor', function () {
+
+    it('should set the context properly', function () {
+      let context = new ExtensionContext();
+      let cache = new Cache(context);
+      assert.equal(cache.context, context);
+    });
+
+    it('should set the namespace properly', function () {
+      let context = new ExtensionContext();
+      let namespace = 'foo';
+      let cache = new Cache(context, namespace);
+      assert.equal(cache.namespace, namespace);
+    });
+
+    it('should set the cache as an empty object', function () {
+      let context = new ExtensionContext();
+      let cache = new Cache(context);
+      assert.deepEqual(cache.cache, {});
+    });
+
+  });
+
+  describe('put', function () {
+
+    it('should set the value in the cache', function () {
+      let context = new ExtensionContext();
+      let cache = new Cache(context);
+      let key = 'foo';
+      let value = 'bar';
+      return cache.put(key, value)
+        .then(() => {
+          assert.equal(cache.get(key), value);
+        });
+    });
+
+    it('returned Promise should resolve to false if a numeric key is provided', function () {
+      let context = new ExtensionContext();
+      let cache = new Cache(context);
+      return cache.put(100, 'bar')
+        .then((success) => {
+          assert.equal(success, false);
+        });
+    });
+
+    it('returned Promise should resolve to false if an object key is provided', function () {
+      let context = new ExtensionContext();
+      let cache = new Cache(context);
+      let obj = {};
+      return cache.put(obj, 'bar')
+        .then((success) => {
+          assert.equal(success, false);
+        });
+    });
+
+    it('returned Promise should resolve to false if a boolean key is provided', function () {
+      let context = new ExtensionContext();
+      let cache = new Cache(context);
+      return cache.put(true, 'bar')
+        .then((success) => {
+          assert.equal(success, false);
+        });
+    });
+
+  });
+
+});
